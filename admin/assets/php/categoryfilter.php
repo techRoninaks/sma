f0b59f5344a8763520252d217d5f44cf527ed934
geethu dwilip <geethudwilip@roninaks.com>
2019-10-17 18:09:38 +0530

    changed categories php, delete php,and seller management.html

--- admin/assets/php/categoryfilter.php
+++ admin/assets/php/categoryfilter.php
@@ -2,7 +2,7 @@
    require "init.php";
    $category=$_POST['category'];
    $success = "successful";
-   $sql_query = "select s.id,s.seller_name from smausr.seller s where s.id in ( select sd.seller_id from smausr.shop_details sd where sd.id in ( SELECT p.shop_id FROM smapr.category c , smapr.product p WHERE lower (c.category) = lower('$category') AND FIND_IN_SET( c.category_id, p.sub_catgry_id) OR p.category_id = (select y.category_id from smapr.category y where lower (y.category) = lower('$category'))))";
+   $sql_query = "select s.id,s.seller_name from roninaks_smausr.seller s where s.id in ( select sd.seller_id from roninaks_smausr.shop_details sd where sd.id in ( SELECT p.shop_id FROM roninaks_smapr.category c , roninaks_smapr.product p WHERE lower (c.category) = lower('$category') AND FIND_IN_SET( c.category_id, p.sub_catgry_id) OR p.category_id = (select y.category_id from roninaks_smapr.category y where lower (y.category) = lower('$category'))))";
    $result = mysqli_query($con2, $sql_query);
    $response = array();
    $count = 0;
