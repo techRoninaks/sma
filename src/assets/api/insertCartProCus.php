32c1ade661e8a2f6d3775208a618d75bc2abe170
sonup <sonup@roninaks.com>
2019-10-22 20:43:40 +0530

    dev_sonu_9
    cart commit

--- src/assets/api/insertCartProCus.php
+++ src/assets/api/insertCartProCus.php
@@ -1,6 +1,6 @@
 <?php
   require "init.php";
-  $user_id = $_POST['user_id'];
+  $userId = $_POST['userId'];
   $prodid = $_POST['prodid'];
   $quantity = $_POST['quantity'];
   $variants_chosen = $_POST['variants_chosen'];
@@ -13,18 +13,23 @@
   $delivery_date = $_POST['delivery_date'];
   $require_delivery_date = $_POST['require_delivery_date'];
   $is_ordered = $_POST['is_ordered'];
-  $user_id = $_POST['user_id'];
-  $gift_title = $_POST['gift_title'];
-  $gift_option = $_POST['gift_option'];
+//   $gift_title = $_POST['gift_title'];
+//   $gift_option = $_POST['gift_option'];
+  $varients_choosen = $_POST['varients_choosen'];
+  $varient_price = $_POST['varient_price'];
+  
           $flag = true;
           $result = array();
           $status = "";
-          $sql_query1 = " INSERT INTO 'cart' ('prodid','quantity','varients_choosen','gift_address','gift_note','total_price','discount','varient_price','has_image','delivery_date','require_delivery_date','is_ordered','gift_title','gift_option') VALUES ('$prodid','$quantity','$varients_choosen','$gift_address','$gift_note','$total_price','$discount','$varient_price','$has_image','$delivery_date','$require_delivery_date','$is_ordered','$gift_title','$gift_option')";
-          $sql_query2 = " INSERT INTO 'customer_order' ('prodid','quantity','varients_choosen','gift_address','gift_note','total_price','discount','varient_price','has_image','delivery_date','gift_title','gift_option') VALUES ('$prodid','$quantity','$varients_choosen','$gift_address','$gift_note','$total_price','$discount','$varient_price','$has_image','$delivery_date','$gift_title','$gift_option')";
+          $sql_query1 = " INSERT INTO 'cart' ('prodid','quantity','varients_choosen','gift_address','gift_note','total_price','discount','varient_price','has_image','delivery_date','require_delivery_date','is_ordered','gift_title','gift_option') VALUES ('$prodid','$quantity','$varients_choosen','$gift_address','$gift_note','$total_price','$discount','$varient_price','$has_image','$delivery_date','$require_delivery_date','$is_ordered','wwww','dhdf')";
+          $sql_query2 = " INSERT INTO 'customer_order' ('prodid','quantity','varients_choosen','gift_address','gift_note','total_price','discount','varient_price','has_image','delivery_date','gift_title','gift_option') VALUES ('$prodid','$quantity','$varients_choosen','$gift_address','$gift_note','$total_price','$discount','$varient_price','$has_image','$delivery_date','wwww','dhdf')";
           $sql_query3 = " INSERT INTO 'purchase_order' ('varients_choosen','total_price','delivery_date') VALUES ($varients_choosen',$total_price','$require_delivery_date')";
           $result1 = mysqli_query($con1, $sql_query1);           
           $result2 = mysqli_query($con1, $sql_query2);           
           $result3 = mysqli_query($con1, $sql_query3);
+
+
+
           if(! $result)
           {
               $status="Error";
