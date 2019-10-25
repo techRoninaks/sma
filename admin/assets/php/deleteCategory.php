7f1411ae4aa3c9799ee729adc48fc27bcb82c552
sinithamolcs <sinitha@roninaks.com>
2019-10-25 03:44:59 -0700

    complete design optimisation.

--- admin/assets/php/deleteCategory.php
+++ admin/assets/php/deleteCategory.php
@@ -1,20 +1,20 @@
 <!-- <if(!$result){
         echo "0";
     }  -->
-<?php
+    <?php
 require 'init.php';
 $id = $_POST['id'];
 header("Content-Type: application/json; charset=UTF-8");
 $response = array();
 $data = array();
 $sql_query1 =  "SELECT parentid FROM category WHERE category_id = $id";
-$result1 = mysqli_query($conn,$sql_query1);
+$result1 = mysqli_query($con1,$sql_query1);
 $pid = mysqli_fetch_assoc($result1);
 $pid_val = $pid["parentid"];
 $sql_query2 = "UPDATE category SET parentid = $pid_val WHERE parentid = $id";
-$result2 = mysqli_query($conn,$sql_query2);
+$result2 = mysqli_query($con1,$sql_query2);
 $sql_query3 = "DELETE FROM category WHERE category_id = $id";
-$result3 = mysqli_query($conn,$sql_query3);
+$result3 = mysqli_query($con1,$sql_query3);
 if(!$result3){
 echo "0";
 }
